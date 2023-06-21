e4a4897c060b6a58604adc480f785b4ebbfc2b00
Anna Fiona Plank <annafiona.plank@gmail.com>
2023-06-21 18:11:44 +0200

    got rid of the unnecassery variables, setters and getters in class ScavTrap

--- ex01/ScavTrap.hpp
+++ ex01/ScavTrap.hpp
@@ -6,12 +6,6 @@
 
 class ScavTrap: public ClapTrap
 {
-private:
-	std::string	_Name;
-	int			_HitPoints;
-	int			_EnergyPoints;
-	int			_AttackDamage;
-
 public:
 //con- and destructors
 	ScavTrap();
@@ -19,17 +13,6 @@ public:
 	ScavTrap(const ScavTrap &C);
 	ScavTrap	&operator = (ScavTrap C);
 	~ScavTrap();
-
-//getters
-	std::string	get_Name() const;
-	int			get_HitPoints() const;
-	int			get_EnergyPoints() const;
-	int			get_AttackDamage() const;
-//setters
-	void	set_Name(const std::string name);
-	void	set_HitPoints(const unsigned int hit_points);
-	void	set_EnergyPoints(const unsigned int energy_points);
-	void	set_AttackDamage(const unsigned int attack_damage);
 
 //member-function
 	void	guardGate();
