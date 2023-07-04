e631bb2cdd8f62f2d76da49f1400ca04f84a334e
Anna Fiona Plank <annafiona.plank@gmail.com>
2023-07-04 20:26:00 +0200

    tests are usable now, but have almost no tests

--- ex00/tests/AnimalTests.cpp
+++ ex00/tests/AnimalTests.cpp
@@ -1,40 +1,16 @@
 #include "tests.hpp"
 
-void	actual_constructor_tests(std::streambuf *filebuf, std::streambuf *coutbuf)
+void	animal_constructor_tests(std::streambuf *filebuf, std::streambuf *coutbuf)
 {
-	Animal	Cow("Cow");
+	Animal	cow("Cow");
 
-	compare(filebuf, coutbuf, "getter:", Cow.getType(), "Cow", 'y');
-}
-
-void	constructor_tests()
-{
-	std::streambuf	*filebuf;
-	std::streambuf	*coutbuf;
-	std::ofstream	outfile;
-	std::string		right_output;
-	std::string		my_output;
-
-	outfile.open("my_output/animal.txt", std::ofstream::out);
-	if (!outfile)
-	{
-		std::cout << "Error: could not open file 'my_output/animal.txt'" << std::endl;
-		exit (1);
-	}
-	coutbuf = std::cout.rdbuf();
-	filebuf = outfile.rdbuf();
-
-	std::cout.rdbuf(filebuf);
-	actual_constructor_tests(filebuf, coutbuf);
-	std::cout.rdbuf(coutbuf);
-
-	right_output = get_file_content((char*)"right_output/animal.txt"); //static-cast!!?
-	my_output = get_file_content((char*)"my_output/animal.txt");
-	outfile.close();
-	compare("animal-output:", my_output, right_output, 'y');
+	compare(filebuf, coutbuf, "getter:", cow.getType(), "Cow", 'y');
+	cow.setType("sheep");
+	compare(filebuf, coutbuf, "setter:", cow.getType(), "sheep", 'y');
 }
 
 void    animal_tests()
 {
-	constructor_tests();
+	constructor_tests_framework(&animal_constructor_tests, "my_output/animal.txt", \
+									"right_output/animal.txt", "animal constructors:");
 }
--- ex00/tests/tests.hpp
+++ ex00/tests/tests.hpp
@@ -7,15 +7,20 @@
 # include <stdlib.h> // allowed?
 
 //AnimalTests.cpp
+void	animal_constructor_tests(std::streambuf *filebuf, std::streambuf *coutbuf);
 void	animal_tests();
 
-//tests.cpp
-void	    ok();
-void	    fail();
-void	    compare(const char *test, const int my_output, const int right_output, const char last);
+//compare_functions.cpp
+void		compare(const char *test, const int my_output, const int right_output, const char last);
 void		compare(const char *test, const std::string my_output, const std::string right_output, const char last);
 void		compare(std::streambuf *filebuf, std::streambuf *coutbuf , \
 					const char *test, const int my_output, const int right_output, const char last);
 void		compare(std::streambuf *filebuf, std::streambuf *coutbuf , \
 					const char *test, const std::string my_output, const std::string right_output, const char last);
-std::string	get_file_content(char *right_output_file);
+
+//tests_utils.cpp
+void	    ok();
+void	    fail();
+void		constructor_tests_framework(void (*constructor_tests)(std::streambuf*, std::streambuf*), \
+				const char *my_outfile_name, const char *right_outfile_name, const char *compare_prompt);
+std::string	get_file_content(const char *right_output_file);
