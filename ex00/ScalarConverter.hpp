680d89d73a580934fd61be73163c7a990f01470a
Norrrrbert <annafiona.plank@gmail.com>
2023-10-20 14:14:59 +0200

    ex00 convert should work now, no tests yet

--- ex00/ScalarConverter.hpp
+++ ex00/ScalarConverter.hpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <sstream>
+#include <cstdlib>
 #include <string>
 #include <limits>
 
--- ex00/ScalarConverterPrintUtils.cpp
+++ ex00/ScalarConverterPrintUtils.cpp
@@ -44,19 +44,58 @@ bool	print_char(const std::string str)
 	return false;
 }
 
+
+static void	print_int_and_char(const std::string str, const float f)
+{	
+	std::istringstream	stream_int(str);
+	int		i;
+
+	stream_int >> i;
+	if (stream_int.fail())
+		std::cout << "int:	not possible" << std::endl;
+	else
+		std::cout << "int:	" << static_cast<int>(f) << std::endl;
+
+	if (stream_int.fail() || i < 0 || i > 127)
+		std::cout << "char:	not possible" << std::endl;
+	else if (i >= 32 && i <= 126)
+		std::cout << "char:	" << static_cast<char>(f) << std::endl;
+	else
+		std::cout << "char:	not displayable" << std::endl;
+}
+
 bool	print_float(const std::string str)
 {
-	std::istringstream stream_int(str);
-	std::istringstream stream(str);
-	float	d;
-	int		i;
+	std::istringstream	stream(str);
+	float	f_check;
+	float	f;
 
 	std::cout << "float" << std::endl;
 
-	stream >> d;
+	stream >> f;
 	if (stream.fail())
 		return true;
-	
+
+	print_int_and_char(str, f);
+	f_check = static_cast<int>(f);
+	if (f == f_check)
+	{
+		std::cout << "float:	" << f << ".0f" << std::endl;
+		std::cout << "double:	" << static_cast<double>(f) << ".0" << std::endl;
+	}
+	else
+	{
+		std::cout << "float:	" << f << "f" << std::endl;
+		std::cout << "double:	" << static_cast<double>(f) << std::endl;
+	}
+	return false;
+}
+
+static void	print_int_and_char(const std::string str, const double d)
+{	
+	std::istringstream	stream_int(str);
+	int		i;
+
 	stream_int >> i;
 	if (stream_int.fail())
 		std::cout << "int:	not possible" << std::endl;
@@ -69,20 +108,15 @@ bool	print_float(const std::string str)
 		std::cout << "char:	" << static_cast<char>(d) << std::endl;
 	else
 		std::cout << "char:	not displayable" << std::endl;
-
-	std::cout << "float:	" << d << std::endl;
-	std::cout << "double:	" << static_cast<double>(d) << std::endl;
-	return false;
 }
 
 bool	print_double(const std::string str)
 {
-	std::istringstream stream_float(str);
-	std::istringstream stream_int(str);
-	std::istringstream stream(str);
+	std::istringstream	stream_float(str);
+	std::istringstream	stream(str);
 	double	d;
+	float	f_check;
 	float	f;
-	int		i;
 
 	std::cout << "double" << std::endl;
 
@@ -90,25 +124,23 @@ bool	print_double(const std::string str)
 	if (stream.fail())
 		return true;
 	
-	stream_int >> i;
-	if (stream_int.fail())
-		std::cout << "int:	not possible" << std::endl;
-	else
-		std::cout << "int:	" << static_cast<int>(d) << std::endl;
-
-	if (stream_int.fail() || i < 0 || i > 127)
-		std::cout << "char:	not possible" << std::endl;
-	else if (i >= 32 && i <= 126)
-		std::cout << "char:	" << static_cast<char>(d) << std::endl;
-	else
-		std::cout << "char:	not displayable" << std::endl;
+	print_int_and_char(str, d);
 
 	stream_float >> f;
 	if (stream_float.fail())
 		std::cout << "float:	not possible" << std::endl;
 	else
-		std::cout << "float:	" << static_cast<float>(d) << std::endl;
-
-	std::cout << "double:	" << d << std::endl;
+	{
+		f_check = static_cast<int>(f);
+		if (f == f_check)
+			std::cout << "float:	" << static_cast<float>(d) << ".0f" << std::endl;
+		else
+			std::cout << "float:	" << static_cast<float>(d) << "f" << std::endl;
+	}
+	f_check = static_cast<int>(f);
+	if (f == f_check)
+		std::cout << "double:	" << d << ".0" << std::endl;
+	else
+		std::cout << "double:	" << d << std::endl;
 	return false;
 }
