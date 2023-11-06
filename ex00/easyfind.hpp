06d4c1667e27f03f36facb531acf24384d356c3f
Anna Fiona Plank <annafiona.plank@gmail.com>
2023-11-06 15:15:59 +0100

    finished ex00

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -1,14 +1,43 @@
 #pragma once
 
-#include <exception>
+#include <stdexcept>
+#include <iostream>
 #include <string>
 
 template<typename T>
-int	easyfind(const T cont, const int num) throw(invalid_argument()) {
+int	easyfind(const T container, const int num) throw(std::invalid_argument) {
 
-	for (std::size_t x = 0; x < cont.size(); x++) {
-		if (cont[x] == num)
-			return (cont[x]);
+	for (typename T::const_iterator	i = container.begin(); i != container.end(); i++) {
+		if (*i == num)
+			return (*i);
 	}
-	throw (invalid_argument("num is not in the container"));
+	throw (std::invalid_argument("num is not in the container"));
+}
+
+template<typename T>
+void	check(const int test_nr, const T container, const int num)
+{
+	std::string	red_bold("\033[1;31m");
+	std::string	yellow("\033[0;33m");
+	std::string white("\033[0m");
+	std::string green("\033[0;32m");
+
+
+	int			output;
+
+	try {
+		output = easyfind<T>(container, num);
+	} catch (std::exception &e) {
+		std::cout << test_nr << ": " << e.what() << std::endl;
+		return ;
+	}
+
+	if (output == num)
+		std::cout << green << test_nr << ": OK" << white << std::endl;
+	else
+	{
+		std::cout << red_bold << test_nr << ": FAIL: " << white << std::endl;
+		std::cout << yellow << "expected output:" << white << std::endl << num;
+		std::cout << yellow << "function output:" << white << std::endl << output << std::endl;
+	}
 }
