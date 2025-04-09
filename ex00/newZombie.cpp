9120a105d7041e57c20be29d7332c78398cccdc6
Anna Fiona Plank <annafiona.plank@gmail.com>
2025-04-09 15:00:06 +0200

    Removed all ex0* directories in root directory

--- ex00/newZombie.cpp
+++
@@ -1,8 +0,0 @@
-#include "Zombie.hpp"
-
-Zombie* newZombie( std::string name )
-{
-	Zombie	*zom2 = new Zombie(name);
-
-	return (zom2);
-}
--- ex00/randomChump.cpp
+++
@@ -1,8 +0,0 @@
-#include "Zombie.hpp"
-
-void	randomChump( std::string name )
-{
-	Zombie	zom3(name);
-
-	zom3.announce();
-}
